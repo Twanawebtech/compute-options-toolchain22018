78a6a41293470b28973503cb643eeaf1474272ad
Twana Daniel <twanaazi@ie.ibm.com>
2017-04-19 09:21:46 +0100

    update script

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-################################################################
-# Install dependencies
-################################################################
-echo 'Installing dependencies...'
-sudo apt-get -qq update 1>/dev/null
-sudo apt-get -qq install jq 1>/dev/null
-sudo apt-get -qq install figlet 1>/dev/null
-
-figlet 'Node.js'
-
-echo 'Installing nvm (Node.js Version Manager)...'
-npm config delete prefix
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.2/install.sh | bash > /dev/null 2>&1
-. ~/.nvm/nvm.sh
-
-echo 'Installing Node.js 6.9.1...'
-nvm install 6.9.1 1>/dev/null
-npm install --progress false --loglevel error 1>/dev/null
-
 
 ################################################################
 # And the web app
